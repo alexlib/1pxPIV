8e7272db61911797d7a5ea5c9d0b89bb35d6d2a3
Alex Liberzon <alex.liberzon@gmail.com>
2015-07-05 02:25:15 +0300

    got another version by e-mail

--- PIVsuite v.0.79/run_1pxPIV.m
+++ PIVsuite v.0.79/run_1pxPIV.m
@@ -18,14 +18,14 @@ pivPar.imMask2 = ['../../TestImages/Mask.bmp'];
 % define the maximum displacements, for which the cross-correlation is evaluated
 pivPar.spDeltaXNeg = 4;
 pivPar.spDeltaXPos = 4;
-pivPar.spDeltaYNeg = 11;
-pivPar.spDeltaYPos = 4;
+pivPar.spDeltaYNeg = 4;
+pivPar.spDeltaYPos = 16;   % try re-run with pivPar.spDeltaYPos = 20 for better result
 % define binding (decreases resolution, but averages cross-correlation function from neighbouring pixels,
 % improving accuracy)
 pivPar.spBindX = 4;
-pivPar.spBindY = 16;
+pivPar.spBindY = 8;
 pivPar.spStepX = 4;
-pivPar.spStepY = 16;
+pivPar.spStepY = 8;
 
 
 % set remaining parameters to defaults
@@ -33,12 +33,12 @@ pivPar = pivParams([],pivPar,'defaults1Px');
 
 
 %% Get list of images
-aux1 = dir([imagePath, filesep, '*a.bmp']);
+aux1 = dir([imagePath, filesep, '*0.BMP']);
 for kk = 1:numel(aux1)
     fileList1{kk} = [imagePath, filesep, aux1(kk).name];  %#ok<SAGROW>
 end
 fileList1 = sort(fileList1);
-aux2 = dir([imagePath, filesep, '*b.bmp']);
+aux2 = dir([imagePath, filesep, '*1.BMP']);
 for kk = 1:numel(aux2)
     fileList2{kk} = [imagePath, filesep, aux2(kk).name];  %#ok<SAGROW>
 end
@@ -63,24 +63,19 @@ pivQuiver(pivData1Px,'spV0');
 % plot profile of vertical velocity in the middle
 subplot(2,1,2);
 hold off;
-plot(pivData1Px.spX(25,:),-pivData1Px.spV0(25,:),'r.');
+plot(pivData1Px.spX(25,:),-pivData1Px.spV0(80,:),'r.');
 hold on;
-plot(pivData1Px.spX(25,:),-pivData1Px.spVfit(25,:),'b.');
+plot(pivData1Px.spX(25,:),-pivData1Px.spVfit(80,:),'b.');
 xlabel('x (px)')
 ylabel('displacement (px)');
-
-% plot std of local mean velocity - just to indicate the same velocity is found regardless Y
-plot(pivData1Px.spX(25,:),std(pivData1Px.spV0,1),'r-');
-legend('V from peak maximum','V from peak fit','V_{rms}','Location','SouthEast');
+legend('V from peak maximum','V from peak fit','Location','SouthEast');
 
 %%
 % plot the cross-correlation function
 figure(2);
-pivQuiver(pivData1Px,'crop',[35,135,360,440],'spCC');
+% following line will show cross-correlation function for pixels 0<X<300 and 650<Y<850. Observe sharp peaks on the left
+% centered around zero displacement (low velocity low velocity fluctuation); peaks displaced downward in the center of
+% jet; and peak with shape of inclined ellipsoid (shear layer with Reynolds stress).
+pivQuiver(pivData1Px,'crop',[0,300,650,850],'spCC');
 
 
-%%
-% plot <u'u'>, <v'v'> and <u'v'> profiles
-figure(2);
-subplot(2,1,1);
-pivQuiver(pivData1Px,'spRSuv');
